52935b61d338bf3244d4cd312db77c041f197705
Thor Frohn Smed Pedersen <Thor.frohn@gmail.com>
2025-09-22 17:48:03 +0200

    Færdig done

--- Brugerinddragelse-Turtle/Form1.cs
+++ Brugerinddragelse-Turtle/Form1.cs
@@ -2,7 +2,7 @@
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace Brugerinddragelse_Turtle
+namespace TurtleGraphics
 {
     public partial class Form1 : Form
     {
@@ -14,16 +14,17 @@ namespace Brugerinddragelse_Turtle
             this.Width = 600;
             this.Height = 600;
         }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
             Graphics g = e.Graphics;
 
-            // TODO: Opret en blå pen med tykkelse 3
-            Pen bluePen = new Pen(______, ______);
+            // Opret en blå pen med tykkelse 3
+            Pen bluePen = new Pen(Color.Blue, 3);
 
-            // TODO: Opret en rød pen med tykkelse 2  
-            Pen redPen = new Pen(______, ______);
+            // Opret en rød pen med tykkelse 2  
+            Pen redPen = new Pen(Color.Red, 2);
 
             // Firkantens position og størrelse
             int squareX = 150;
@@ -35,11 +36,11 @@ namespace Brugerinddragelse_Turtle
             int circleY = 200;
             int circleSize = 80;
 
-            // TODO: Tegn firkanten med DrawRectangle
-            g.DrawRectangle(______, ______, ______, ______, ______);
+            // Tegn firkanten med DrawRectangle
+            g.DrawRectangle(bluePen, squareX, squareY, squareSize, squareSize);
 
-            // TODO: Tegn cirklen med DrawEllipse
-            g.DrawEllipse(______, ______, ______, ______, ______);
+            // Tegn cirklen med DrawEllipse
+            g.DrawEllipse(redPen, circleX, circleY, circleSize, circleSize);
         }
     }
 }
